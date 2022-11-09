9486e8192bb3472dc1f0f7f9965b5a7f8fc57e83
Antoine D <a.desfaits@orange.fr>
2022-11-09 20:39:27 +0100

    First commit with the form

---
+++ SolarDimension/SolarDimension.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SolarDimension.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
