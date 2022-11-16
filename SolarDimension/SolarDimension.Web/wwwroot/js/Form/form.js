2d1dfe68488d523119ff802d579a5868def216fe
Antoine D <a.desfaits@orange.fr>
2022-11-16 20:21:33 +0100

    try to add map

--- SolarDimension/SolarDimension.Web/wwwroot/js/Form/form.js
+++ SolarDimension/SolarDimension.Web/wwwroot/js/Form/form.js
@@ -1,6 +1,24 @@
 ﻿var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
+// Map
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+
+
+
+
+
+
+// Functions for tabs
+
 function showTab(n) {
     //This function will display the specified tab of the form ...
     var x = document.getElementsByClassName("tab");
