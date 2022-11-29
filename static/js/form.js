e5613d01a020131adbb71a1b0ef08609821738e8
Ant Desf <a.desfaits@orange.fr>
2022-11-29 21:42:34 +0100

    bug fix

--- static/js/form.js
+++ static/js/form.js
@@ -1,16 +1,6 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
-// Map
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-
 
 
 // Functions for tabs
